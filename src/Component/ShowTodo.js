7ca246e638f6816ca23c79cf2c41e41a272f44e9
Oralita <oat431@gmail.com>
2021-06-10 19:38:20 +0700

    [ ✨ update ] : the main functionality

--- src/Component/ShowTodo.js
+++ src/Component/ShowTodo.js
@@ -1,42 +1,62 @@
-import { Container, ListGroup } from "react-bootstrap";
-import React, { Component } from "react";
-// import Axios from "axios";
-class ShowTodo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todo_list: props.todo,
-    };
-  }
+import {
+  Container,
+  ListGroup,
+  Button,
+  Row,
+  Col,
+  ButtonGroup,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-  // componentDidMount() {
-  //   Axios.get("http://167.99.64.31:5039/api/todo/todolist", {
-  //     header: { "Access-Control-Allow-Origin": "*" },
-  //   })
-  //     .then((res) => {
-  //         console.log(res.data);
-  //         this.setState({ todo_list: res.data });
-  //     })
-  //     .catch((err) => {
-  //       console.log("some err happen");
-  //     });
-  // }
-
-  render() {
-    return (
-      <Container>
-        <ListGroup as="ul">
-          {this.state.todo_list.map((item, index) => {
-            return (
-              <ListGroup.Item as="li">
-                <strong>{index + " " + item}</strong> 
-              </ListGroup.Item>
-            );
-          })}
-        </ListGroup>
-      </Container>
-    );
+function ShowTodo(props) {
+  let deleting = (elem) => {
+    let name = props.todo.indexOf(elem.target.name);
+    props.todo.splice(name,1);
+  };
+  let editing = (elem) => {
+    let newName = promps();
+    let name = props.todo.indexOf(elem.target.name);
+    props.todo[name] = newName;
   }
+  return (
+    <Container>
+      <ListGroup as="ul">
+        {props.todo.map((item, index) => {
+          return (
+            <ListGroup.Item as="li">
+              <Container>
+                <Row>
+                  <Col xs={10} md={10} xl={10}>
+                    <strong>{index + 1 + " " + item}</strong>
+                  </Col>
+                  <Col
+                    xs={2}
+                    md={2}
+                    xl={2}
+                    className="d-flex justify-content-end"
+                  >
+                    <ButtonGroup>
+                      <Button variant="warning" size="md">
+                        edit
+                      </Button>
+                      <Button
+                        variant="danger"
+                        size="md"
+                        name={item}
+                        onClick={deleting}
+                      >
+                        delete
+                      </Button>
+                    </ButtonGroup>
+                  </Col>
+                </Row>
+              </Container>
+            </ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+    </Container>
+  );
 }
 
 export default ShowTodo;
